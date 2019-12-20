27fbecc747eebe129452658f396a89afb2813c39
Potassium Isotope 32 <40513541+AcerOne10@users.noreply.github.com>
2019-12-20 14:25:14 +0530

    Incremental Fix 3 to Issue #1

--- Main.java
+++ Main.java
@@ -1,8 +1,42 @@
+/*
+ * Part of Rubidium Projects
+ * General Public Licence v3.0, 2019
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+ /**
+  * Basic Start-Point for Program
+  *
+ **/
 public class Main
 {
 	public static void main(String[] args) {
+		// Get Path
 		Settings.getPath();
+		
+		// Global Init Method
 		Global.startProgram();
+		
+		// Program Loop
 		while(Global.isRunning()) {
 			System.out.print("\n "+Settings.path + " >> ");
 			String com = Global.getInput();
